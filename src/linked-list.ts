a968c0851284ce7da0b56e84688a5164f96627b2
vadimkononenko <vadiskon@gmail.com>
2022-04-17 23:30:37 +0300

    added implementation of Singly Linked List and UNIT-tests

---
+++ src/linked-list.ts
@@ -0,0 +1,256 @@
+class ListNode {
+  private _value: number;
+  public get value(): number {
+    return this._value;
+  }
+  public set value(v: number) {
+    this._value = v;
+  }
+
+  private _next: ListNode | null;
+  public get next(): ListNode | null {
+    return this._next;
+  }
+  public set next(v: ListNode | null) {
+    this._next = v;
+  }
+
+  constructor(nodeValue: number) {
+    this._value = nodeValue;
+    this._next = null;
+  }
+}
+
+export class SinglyLinkedList {
+  private _head: ListNode | null;
+  public get head(): ListNode | null {
+    return this._head;
+  }
+  private set head(node: ListNode | null) {
+    this._head = node;
+  }
+
+  private _tail: ListNode | null;
+  public get tail(): ListNode | null {
+    return this._tail;
+  }
+  private set tail(node: ListNode | null) {
+    this._tail = node;
+  }
+
+  private _listLength: number;
+  public get listLength(): number {
+    return this._listLength;
+  }
+  private set listLength(v: number) {
+    this._listLength = v;
+  }
+
+  constructor() {
+    this._listLength = 0;
+    this._head = null;
+    this._tail = null;
+  }
+
+  public length(): number {
+    return this._listLength;
+  }
+
+  public append(element: number): void {
+    let newNode = new ListNode(element);
+
+    if (this._head === null) {
+      this._head = newNode;
+      this._tail = this._head;
+    } else {
+      this._tail!.next = newNode;
+      this._tail = newNode;
+    }
+
+    this._listLength++;
+  }
+
+  public insert(element: number, index: number): void {
+    let node = this.get(index);
+
+    if (node) {
+      node.value = element;
+    }
+  }
+
+  public remove(index: number): number {
+    if (index < 0 || index > this._listLength - 1) {
+      throw Error(`Can't get element with index ${index}`);
+    } else if (index === 0) {
+      return this.shift();
+    } else if (index === this._listLength - 1) {
+      return this.pop();
+    } else {
+      const prevNode = this.get(index - 1);
+      const removeNode = prevNode!.next;
+
+      prevNode!.next = removeNode!.next;
+      removeNode!.next = null;
+
+      this._listLength--;
+
+      return removeNode!.value;
+    }
+  }
+
+  public removeAll(element: number): void {
+    for (let i = this.length() - 1; i >= 0; i--) {
+      if (this.get(i)!.value === element) this.remove(i);
+    }
+  }
+
+  public get(index: number): ListNode | null {
+    if (this._listLength === 0 || index > this._listLength - 1 || index < 0) {
+      throw Error(`Can't get element with index ${index}`);
+    } else {
+      let returnNode: ListNode | null = this._head;
+      for (let i = 1; i <= index; i++) {
+        returnNode = returnNode!.next;
+      }
+
+      return returnNode;
+    }
+  }
+
+  public clone(): SinglyLinkedList | null {
+    let newList: SinglyLinkedList | null = new SinglyLinkedList();
+    for (let i = 0; i < this.length(); i++) {
+      newList!.append(this.get(i)!.value);
+    }
+    return newList;
+  }
+
+  public reverse() {
+    let currentNode = this._head;
+    this._head = this._tail;
+    this._tail = currentNode;
+
+    let prevNode: ListNode | null = null;
+    let nextNode: ListNode | null;
+
+    for(let i = 0; i < this._listLength; i++) {
+      nextNode = currentNode!.next;
+      currentNode!.next = prevNode;
+      prevNode = currentNode;
+      currentNode = nextNode;
+    }
+  }
+
+  public findFirst(element: number): number {
+    let current: ListNode | null = this._head;
+    let currentPosition: number = 0;
+    while(current) {
+      if(current.value === element) {
+        return currentPosition;
+      }
+      currentPosition++;
+      current = current.next;
+    }
+    return -1;
+  }
+
+  public findLast(element: number): number {
+    let current: ListNode | null = this._head;
+    let currentPosition: number = 0;
+    let res: number = 0;
+    while(current) {
+      if(current.value === element) {
+        res = currentPosition;
+      }
+      currentPosition++;
+      current = current.next;
+    }
+
+    if(res) return res;
+    else return -1;
+  }
+
+  public clear() {
+    this._head = null;
+    this._tail = null;
+    this.listLength = 0;
+  }
+
+  public extend(list: SinglyLinkedList): void {
+    for (let i = 0; i < list.length(); i++) {
+      this.append(list.get(i)!.value);
+    }
+  }
+
+  private shift(): number {
+    if (this._listLength === 0) {
+      throw Error('List is empty');
+    } else if (this._head === this._tail) {
+      const returnNode = this._head;
+      this._head = null;
+      this._tail = null;
+      this._listLength = 0;
+
+      return returnNode!.value;
+    } else {
+      const prevHeadNode = this._head;
+      this._head = prevHeadNode!.next;
+      prevHeadNode!.next = null;
+      this._listLength--;
+
+      return prevHeadNode!.value;
+    }
+  }
+
+  private pop(): number {
+    if (this._listLength === 0) {
+      throw Error('List is empty');
+    } else if (this._head === this._tail) {
+      const returnNode: ListNode | null = this._head;
+      this._head = null;
+      this._tail = null;
+      this._listLength = 0;
+
+      return returnNode!.value;
+    } else {
+      let returnNode: ListNode | null;
+      let newTailNode: ListNode | null = null;
+      let currentNode = this._head;
+
+      while (currentNode!.next) {
+        newTailNode = currentNode;
+        currentNode = currentNode!.next;
+      }
+
+      returnNode = this._tail;
+      newTailNode!.next = null;
+      this._tail = newTailNode;
+      this._listLength--;
+
+      return returnNode!.value;
+    }
+  }
+}
+
+let list1: SinglyLinkedList = new SinglyLinkedList();
+let list2: SinglyLinkedList = new SinglyLinkedList();
+
+list1.append(1);
+list1.append(2);
+list1.append(3);
+
+list2.append(4);
+list2.append(5);
+list2.append(6);
+
+list1.extend(list2);
+
+for (let i = 0; i < list1.length(); i++) {
+  console.log(list1.get(i)?.value);
+}
+
+console.log(list1.length());
+
+list2.append(5324);
+
+console.log(list1.length());
---
+++ test/linked-list.test.ts
@@ -0,0 +1,246 @@
+import {SinglyLinkedList} from "../src/linked-list";
+
+const mockData = {
+  emptyList: {
+    _listLength: 0,
+    _head: null,
+    _tail: null},
+  appendedDataToList: {
+    _listLength: 1,
+    _head: { _value: 1, _next: null },
+    _tail: { _value: 1, _next: null }
+  },
+  getByIndex: {
+    _value: 2,
+    _next: { _value: 3, _next: null } },
+  clonedList: {
+    _listLength: 3,
+    _head: { _value: 4, _next: { _value: 2231, _next: { _value: 53, _next: null } }},
+    _tail: { _value: 53, _next: null }
+  },
+  clonedListWithoutChanges: {
+    _listLength: 3,
+    _head: { _value: 1, _next: { _value: 2, _next: { _value: 3, _next: null } }},
+    _tail: { _value: 3, _next: null }
+  },
+  reversedList: {
+    _listLength: 3,
+    _head: {
+      _value: 45743,
+      _next: { _value: 867, _next: { _value: 23, _next: null } }
+    },
+    _tail: { _value: 23, _next: null }
+  },
+};
+
+describe('Testing Singly Linked List', () => {
+  it('should create linked list', () => {
+    expect(new SinglyLinkedList()).toEqual(mockData.emptyList);
+  });
+
+  it('should return 0 for length()', () => {
+    expect(new SinglyLinkedList().length()).toBe(0);
+  });
+
+  it('should add element to the end of the list', () => {
+    const list: SinglyLinkedList = new SinglyLinkedList();
+    list.append(1);
+
+    expect(list).toEqual(mockData.appendedDataToList);
+  });
+
+  it('should insert element by index at first place', () => {
+    const list: SinglyLinkedList = new SinglyLinkedList();
+    list.append(1);
+
+    list.insert(1111, 0);
+
+    expect(list.findFirst(1111)).toEqual(0);
+  });
+
+  it('should insert element by index', () => {
+    const list: SinglyLinkedList = new SinglyLinkedList();
+    list.append(1);
+    list.append(2);
+    list.append(3);
+    list.append(4);
+    list.append(5);
+
+    list.insert(2001, 2);
+
+    expect(list.get(2)!.value).toEqual(2001);
+  });
+
+  it('should not insert element by index which is not exist', () => {
+    const list: SinglyLinkedList = new SinglyLinkedList();
+    list.append(1);
+    list.append(2);
+    list.append(3);
+
+    expect(() => {list.insert(2001, 3)}).toThrow('Can\'t get element with index 3');
+  });
+
+  it('should not delete element by negative index', () => {
+    const list: SinglyLinkedList = new SinglyLinkedList();
+    list.append(1);
+    list.append(2);
+    list.append(3);
+
+    expect(() => {list.remove(-1)}).toThrow('Can\'t get element with index -1');
+  });
+
+  it('should not delete element by out of bound index', () => {
+    const list: SinglyLinkedList = new SinglyLinkedList();
+    list.append(1);
+    list.append(2);
+    list.append(3);
+
+    expect(() => {list.remove(5)}).toThrow('Can\'t get element with index 5');
+  });
+
+  it('should delete element by index', () => {
+    const list: SinglyLinkedList = new SinglyLinkedList();
+    list.append(1);
+    list.append(2);
+    list.append(3);
+
+    expect(list.remove(1)).toEqual(2);
+    expect(list.length()).toEqual(2);
+  });
+
+  it('should delete all matches with element', () => {
+    const list: SinglyLinkedList = new SinglyLinkedList();
+    list.append(1);
+    list.append(2);
+    list.append(3);
+    list.append(3);
+    list.append(3);
+    list.append(4);
+    list.append(5);
+
+    list.removeAll(3);
+
+    expect(list.length()).toEqual(4);
+    expect(list.get(2)!.value).toEqual(4);
+    expect(list.get(3)!.value).toEqual(5);
+  });
+
+  it('should not change list by delete element which is not exist', () => {
+    const list: SinglyLinkedList = new SinglyLinkedList();
+    list.append(1);
+    list.append(2);
+    list.append(3);
+    list.append(4);
+    list.append(5);
+
+    const listLengthBeforeRemoving: number = list.length();
+
+    list.removeAll(6);
+    expect(list.length()).toEqual(listLengthBeforeRemoving);
+  });
+
+  it('should return singly linked object by index', () => {
+    const list: SinglyLinkedList = new SinglyLinkedList();
+    list.append(1);
+    list.append(2);
+    list.append(3);
+
+    expect(list.get(1)).toEqual(mockData.getByIndex);
+  });
+
+  it('should throw exception by using incorrect index', () => {
+    const list: SinglyLinkedList = new SinglyLinkedList();
+    list.append(1);
+    list.append(2);
+    list.append(3);
+
+    expect(() => {list.get(5)}).toThrow('Can\'t get element with index 5');
+    expect(() => {list.get(-1)}).toThrow('Can\'t get element with index -1');
+  });
+
+  it('should successfully clone linked list', () => {
+    const listFirst: SinglyLinkedList = new SinglyLinkedList();
+    listFirst.append(4);
+    listFirst.append(2231);
+    listFirst.append(53);
+
+    let clonedList: SinglyLinkedList | null;
+
+    clonedList = listFirst.clone();
+
+    expect(clonedList!.length()).toEqual(3);
+    expect(clonedList).toEqual(mockData.clonedList);
+  });
+
+  it('should successfully reverse linked list', () => {
+    const list: SinglyLinkedList = new SinglyLinkedList();
+    list.append(23);
+    list.append(867);
+    list.append(45743);
+
+    list.reverse();
+
+    expect(list!.length()).toEqual(3);
+    expect(list).toEqual(mockData.reversedList);
+  });
+
+  it('should find first element and return index', () => {
+    const list: SinglyLinkedList = new SinglyLinkedList();
+    list.append(9865);
+    list.append(674);
+    list.append(3025);
+    list.append(674);
+    list.append(674);
+
+    expect(list.findFirst(674)).toEqual(1);
+  });
+
+  it('should find last element and return index', () => {
+    const list: SinglyLinkedList = new SinglyLinkedList();
+    list.append(9865);
+    list.append(674);
+    list.append(3025);
+    list.append(674);
+    list.append(674);
+
+    expect(list.findLast(674)).toEqual(4);
+  });
+
+  it('should clear linked list', () => {
+    const list: SinglyLinkedList = new SinglyLinkedList();
+    list.append(53);
+    list.append(4);
+    list.append(122);
+
+    list.clear();
+
+    expect(list!.length()).toEqual(0);
+    expect(list).toEqual(mockData.emptyList);
+  });
+
+  it('should list extend another list', () => {
+    let listF: SinglyLinkedList = new SinglyLinkedList();
+    let listS: SinglyLinkedList = new SinglyLinkedList();
+
+    listF.append(1);
+    listF.append(2);
+    listF.append(3);
+
+    listS.append(4);
+    listS.append(5);
+    listS.append(6);
+
+    listF.extend(listS);
+
+    expect(listF!.length()).toEqual(6);
+    expect(listF.get(3)!.value).toEqual(4);
+  });
+});
+
+
+
+
+
+
+
+
